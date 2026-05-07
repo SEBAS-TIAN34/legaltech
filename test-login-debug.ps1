2d755367916b6e495f0cf9a9f3d9f632bc342b0f
Sebastian <sebastian@legaltech.com>
2026-05-07 01:22:45 -0500

    chore: Remove unused test files and clean up project

--- test-login-debug.ps1
+++
@@ -1,4 +0,0 @@
-$loginBody = @{ email = "admin1201710980@test.com"; password = "admin123" } | ConvertTo-Json
-
-$login = Invoke-RestMethod -Uri "http://localhost:3001/api/auth/login" -Method POST -Body $loginBody -ContentType "application/json"
-$login | ConvertTo-Json -Depth 5
